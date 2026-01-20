0053654d543070ec9eb48d2883f628c3ab08c820
AkshaySG <akshay.sg55@gmail.com>
2026-01-20 12:45:43 +0000

    overflow of ruler fix

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -75,11 +75,11 @@ export function Ruler() {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
-      className="h-6 border border-gray-300 flex items-end relative select-none print:hidden"
+      className="w-[816px] mx-auto h-6 border border-gray-300 flex items-end relative select-none print:hidden"
     >
       <div
         id="ruler-container"
-        className="max-w-[816px] mx-auto w-full h-full relative"
+        className="w-full h-full relative"
       >
         <Marker
           postion={leftMargin}
