a8ba39aa32aa58d5a2eae95c925a94b22be27dc5
AkshaySG <akshay.sg55@gmail.com>
2026-01-20 18:20:25 +0000

    adding search and mainpage navigation

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,23 @@
+// &nbsp; is unicode for whitespace
+
+import Link from "next/link";
+import { Navbar } from "./navbar";
+
+function Page() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+        <Navbar />
+      </div>
+      <div className="mt-16">
+        Click{" "}
+        <Link href={"/documents/123"}>
+          <span className="text-blue-800 underline">&nbsp;here&nbsp;</span>
+        </Link>
+        to go to documentsId
+      </div>
+    </div>
+  );
+}
+
+export default Page;
